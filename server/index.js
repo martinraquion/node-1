4bd550501080f7924182d2ae6650b2a1b6da4420
Martin Raquion <raquionearl@gmail.com>
2019-07-22 17:05:18 +0800

    node-1 submisson

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const products = require('../products.json');
+
+const app = express();
+
+const port = 3001;
+
+app.get('/api/products', (req, res) => {
+res.status(200).send(products);
+});
+
+app.listen(port, () => {
+console.log(`Server listening on port: ${port}`);
+});
